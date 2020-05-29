cedf875b2d188d9b416d975b055383577798c833
Robson Lima <rsl.robson@gmail.com>
2020-05-28 23:33:50 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/rdev/hmtimeturner/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.rdev.hmtimeturner.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.rdev.hmtimeturner.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
